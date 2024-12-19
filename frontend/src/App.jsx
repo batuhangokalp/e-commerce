1946412a31331a4918718ef0c5977cb25a8a94f0
Batuhan Gokalp <bbatuhangokalp@gmail.com>
2024-12-19 18:43:41 +0300

    created home page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,12 @@
-import Categories from "./components/Categories/Categories";
-import Footer from "./components/Layout/Footer/Footer";
-import Header from "./components/Layout/Header/Header";
-import Policy from "./components/Layout/Policy/Policy";
-import Products from "./components/Products/Products";
-import Sliders from "./components/Slider/Sliders";
-import Campaigns from "./components/Campaigns/Campaigns";
-import Blogs from "./components/Blogs/Blogs";
+import React from "react";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Sliders />
-      <Categories />
-      <Products />
-      <Campaigns />
-      <Blogs />
-      <Policy />
-      <Footer />
-    </>
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
   );
 }
 
