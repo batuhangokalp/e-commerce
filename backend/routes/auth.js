522adf91fdbe9076fdc891e259bec1a07aaf2d52
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2024-12-27 15:08:48 +0300

    created register and login process

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,10 +47,16 @@ router.post("/login", async (req, res) => {
 
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ error: "Invalid email or password!" });
+      return res.status(401).json({ error: "Invalid email!" });
     }
 
-    res.status(200).json({message: "Login is successful!"})
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password!" });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Server error!" });
