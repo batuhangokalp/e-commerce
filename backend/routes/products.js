8432578f6d5a3258b273d063fbd87c46b0a1d3b0
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2025-01-05 17:40:38 +0300

    created product search process

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -85,4 +85,20 @@ router.delete("/:productId", async (req, res) => {
 });
 // #endregion
 
+// #region Searching product according to name
+router.get("/search/:productName", async (req, res) => {
+  try {
+    const productName = req.params.productName;
+    const products = await Product.find({
+      name: { $regex: productName, $options: "i" },
+    });
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error!" });
+  }
+});
+// #endregion
+
 module.exports = router;
